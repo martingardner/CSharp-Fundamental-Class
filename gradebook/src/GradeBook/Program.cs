11281fd181f4cc655ed67a29a032857d929ecfb6
martin gardner II <martingardnerii@gmail.com>
2020-08-04 13:15:39 -0500

    current place in lesson 3

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -4,9 +4,40 @@ namespace GradeBook
 {
     class Program
     {
+        // dotnet run -- Martin
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            var numbers = new [] { 12.7, 10.3, 6.11};
+            var result = 0.0;
+            foreach(var number in numbers)
+            {
+                result += number;
+            }
+
+            Console.WriteLine(result);
+
+            /*
+            Console.WriteLine(numbers[0] + numbers[1] + numbers[2]);
+            */
+            /*
+            var x = 34.1;
+            var y = 35.9;            
+
+            Console.WriteLine(x + y);
+            */
+            /*
+
+
+            if(args.Length > 0)
+            {
+                Console.WriteLine($"Hello,  { args[0] } !");
+            }
+            else 
+            {
+                Console.WriteLine("Hello");
+            }
+            */
         }
     }
 }
