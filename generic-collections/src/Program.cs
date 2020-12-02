f8c276247a6226368a620bb77293dd0772bda533
martin gardner II <martingardnerii@gmail.com>
2020-12-02 12:00:05 -0600

    generic-collection folder try out

---
+++ generic-collections/src/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace src
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            /********** Dictionary **************/
+            var dict = new Dictionary<string, object>();
+
+            dict.Add("Scott", 115);
+            dict.Add("Bob", 118);
+            dict.Add("Clarence", 2174);
+
+            //searches through dictionary looking for first Scott reference
+            var Scott = dict["Scott"];
+
+            Console.WriteLine(Scott);
+            foreach (var item in dict)
+            {
+                Console.WriteLine("{0} and {1}", item.Key, item.Value);
+            }
+            /********** Dictionary End *********/
+
+        }
+    }
+}
